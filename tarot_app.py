7c3d348d7a5fe83e937ec70f6cf82254945965fb
wangxinyi921-arch <wangxinyi921@gmail.com>
2025-10-27 17:23:30 -0500

    Add tarot card reading app using Streamlit

---
+++ tarot_app.py
@@ -0,0 +1,36 @@
+import streamlit as st
+import random
+
+st.title("🔮 塔罗抽牌占卜")
+
+tarot_cards = [
+    "The Fool", "The Magician", "The High Priestess", "The Empress", "The Emperor",
+    "The Hierophant", "The Lovers", "The Chariot", "Strength", "The Hermit",
+    "Wheel of Fortune", "Justice", "The Hanged Man", "Death", "Temperance",
+    "The Devil", "The Tower", "The Star", "The Moon", "The Sun",
+    "Judgement", "The World",
+    "Ace of Wands", "Two of Wands", "Three of Wands", "Four of Wands", "Five of Wands",
+    "Six of Wands", "Seven of Wands", "Eight of Wands", "Nine of Wands", "Ten of Wands",
+    "Page of Wands", "Knight of Wands", "Queen of Wands", "King of Wands",
+    "Ace of Cups", "Two of Cups", "Three of Cups", "Four of Cups", "Five of Cups",
+    "Six of Cups", "Seven of Cups", "Eight of Cups", "Nine of Cups", "Ten of Cups",
+    "Page of Cups", "Knight of Cups", "Queen of Cups", "King of Cups",
+    "Ace of Swords", "Two of Swords", "Three of Swords", "Four of Swords", "Five of Swords",
+    "Six of Swords", "Seven of Swords", "Eight of Swords", "Nine of Swords", "Ten of Swords",
+    "Page of Swords", "Knight of Swords", "Queen of Swords", "King of Swords",
+    "Ace of Pentacles", "Two of Pentacles", "Three of Pentacles", "Four of Pentacles", "Five of Pentacles",
+    "Six of Pentacles", "Seven of Pentacles", "Eight of Pentacles", "Nine of Pentacles", "Ten of Pentacles",
+    "Page of Pentacles", "Knight of Pentacles", "Queen of Pentacles", "King of Pentacles"
+]
+
+tarot_pool = tarot_cards + [card + " (Reversed)" for card in tarot_cards]
+
+question = st.text_input("✨ 你的问题是什么？")
+num_cards = st.slider("你想抽几张牌？", 1, 6, 1)
+
+if st.button("🔮 抽牌！"):
+    chosen_cards = random.sample(tarot_pool, num_cards)
+    st.markdown("---")
+    st.subheader("你抽到的牌是：")
+    for i, card in enumerate(chosen_cards, 1):
+        st.write(f"**{i}. {card}**")
